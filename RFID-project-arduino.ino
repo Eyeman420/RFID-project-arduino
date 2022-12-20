20d8bd7641238db4750acff36f17e3581188ad54
Freezanator <freezanator95@yahoo.com>
2022-12-20 20:46:00 +0800

    Update RFID-project-arduino.ino

--- RFID-project-arduino.ino
+++ RFID-project-arduino.ino
@@ -1,74 +1,70 @@
-//Viral Science www.viralsciencecreativity.com www.youtube.com/c/viralscience
-//RFID Door Lock System
-
 #include <Wire.h>
 #include <SPI.h>
 #include <MFRC522.h>
+#include <Servo.h>
 #include <LiquidCrystal_I2C.h> 
-LiquidCrystal_I2C lcd(0x3F, 16, 2);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
  
 #define SS_PIN 10
 #define RST_PIN 9
-#define LED_G 4 //define green LED pin
-#define LED_R 5 //define red LED
-#define BUZZER 2 //buzzer pin
-#define lock 3
-MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
-int Btn = 6;
- 
-void setup() 
-{
-  Serial.begin(9600);   // Initiate a serial communication
-  SPI.begin();      // Initiate  SPI bus
-  mfrc522.PCD_Init();   // Initiate MFRC522
-  pinMode(LED_G, OUTPUT);
-  pinMode(LED_R, OUTPUT);
-  pinMode(BUZZER, OUTPUT);
-  noTone(BUZZER);
-  pinMode(Btn,INPUT);
-  pinMode(lock,OUTPUT);
+#define LED_G 3                     // Define green LED as pin 3
+#define LED_R 2                     // Define red LED as pin 2
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
+int button = 4;                     // Set button on pin 4
+Servo servo;                        // Declare servo
+
+String registered[4] = {"80 91 0D 7C", "F0 59 FE 73"};
+
+void unlock() {
+
+  Serial.println("Access Granted");
+  Serial.println();
+  delay(500);
+  digitalWrite(LED_G, HIGH);
+  lcd.setCursor(0,1);
+  lcd.print("  Door Unlocked ");
+  servo.write(90);
+
+  // Automatically locks door after 3 seconds
+  delay(3000);
+  servo.write(15);
+  delay(100);
+  digitalWrite(LED_G, LOW);
+  lcd.setCursor(0,1);
+  lcd.print("   Door Locked   ");  
+}
+
+void setup() {
+
+  Serial.begin(9600);     // Initiate a serial communication
+  SPI.begin();            // Initiate  SPI bus
+  mfrc522.PCD_Init();     // Initiate MFRC522
+  pinMode(LED_G, OUTPUT); // Set green LED as OUTPUT
+  pinMode(LED_R, OUTPUT); // Set red LED as OUTPUT
+  pinMode(button, INPUT); // Set button as INPUT
+  servo.attach(6);        // Set servo on pin 6
+  servo.write(15);        // Set servo angle at 15 degrees
+  
   Serial.println("Place your card on reader...");
   Serial.println();
   lcd.begin();
   lcd.backlight();
   lcd.clear();
-  lcd.setCursor(0,0); // column, row
+  lcd.setCursor(0,0); // Column, Row
   lcd.print(" Scan Your RFID "); 
-  lcd.setCursor(0,1); // column, row
+  lcd.setCursor(0,1); // Column, Row
   lcd.print("   Door Locked   ");
+}
 
- }
-void loop() 
-{
+void loop() {
 
-if(digitalRead(Btn) == HIGH){
+  bool flag = false;
+
+  // Unlocks door if button is pressed
+  if(digitalRead(button) == HIGH){
   
-    Serial.println("Access Granted");
-    Serial.println();
-    delay(500);
-    digitalWrite(LED_G, HIGH);
-    lcd.setCursor(0,1); // column, row
-    lcd.print(" Door Un-Locked ");
-    tone(BUZZER, 2000);
-    delay(100);
-    noTone(BUZZER);
-    delay(50);
-    tone(BUZZER, 2000);
-    delay(100);
-    noTone(BUZZER);
-    digitalWrite(lock,HIGH);
-    delay(3000);
-    digitalWrite(lock,LOW);
-    delay(100);
-    digitalWrite(LED_G, LOW);
-    lcd.setCursor(0,1); // column, row
-    lcd.print("   Door Locked   ");
-    tone(BUZZER, 2000);
-    delay(100);
-    noTone(BUZZER);
-  delay(50);
+   unlock();
   }
-
 
   // Look for new cards
   if ( ! mfrc522.PICC_IsNewCardPresent()) 
@@ -80,74 +76,56 @@ if(digitalRead(Btn) == HIGH){
   {
     return;
   }
-  //Show UID on serial monitor
-  Serial.print("UID tag :");
+
+  // Show ID on serial monitor
+  Serial.print("ID tag :");
   String content= "";
   byte letter;
-  for (byte i = 0; i < mfrc522.uid.size; i++) 
-  {
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+     
      Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
      Serial.print(mfrc522.uid.uidByte[i], HEX);
      content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
      content.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
+
   Serial.println();
   Serial.print("Message : ");
   content.toUpperCase();
 
-
+  // Displays ID
+  lcd.setCursor(0,1);
+  lcd.print("ID: " + content.substring(1) + " ");
+  delay(1500);
   
- if (content.substring(1) == "83 23 38 BB") //change here the UID of card/cards or tag/tags that you want to give access
-  {
-    Serial.println("Access Granted");
-    Serial.println();
-    delay(500);
-    digitalWrite(LED_G, HIGH);
-    lcd.setCursor(0,1); // column, row
-    lcd.print(" Door Un-Locked ");
-    tone(BUZZER, 2000);
-    delay(100);
-    noTone(BUZZER);
-    delay(50);
-    tone(BUZZER, 2000);
-    delay(100);
-    noTone(BUZZER);
-    digitalWrite(lock,HIGH);
-    delay(3000);
-    digitalWrite(lock,LOW);
-    delay(100);
-    digitalWrite(LED_G, LOW);
-    lcd.setCursor(0,1); // column, row
-    lcd.print("   Door Locked   ");
-    tone(BUZZER, 2000);
-    delay(100);
-    noTone(BUZZER);
-    delay(50);
-  }
+  for (int i = 0; i < 4; i ++) {
 
-else
-{
-    lcd.setCursor(0,1); // column, row
+    if (content.substring(1) == registered[i]) {
+
+      flag = true;
+      unlock();
+      break;
+    }
+  }
+  
+  // Denies access if unregistered ID is presented
+  if (flag == false) {
+    
+    lcd.setCursor(0,1);
     lcd.print("Invalid RFID Tag");
-    Serial.println(" Access denied");
+    Serial.println("Access denied");
     digitalWrite(LED_R, HIGH);
-    tone(BUZZER, 1500);
     delay(500);
     digitalWrite(LED_R, LOW);
-    noTone(BUZZER);
     delay(100);
     digitalWrite(LED_R, HIGH);
-    tone(BUZZER, 1500);
     delay(500);
     digitalWrite(LED_R, LOW);
-    noTone(BUZZER);
     delay(100);
     digitalWrite(LED_R, HIGH);
-    tone(BUZZER, 1500);
     delay(500);
     digitalWrite(LED_R, LOW);
-    noTone(BUZZER);
-    lcd.setCursor(0,1); // column, row
+    lcd.setCursor(0,1);
     lcd.print("   Door Locked   ");
- }
- }
+  }
+}
